0777a068a5fcf747352e9f5f65a185d2fd904623
Victor Martinez <vicmarc30@gmail.com>
2026-07-14 11:09:02 -0600

    feat: implement Insights screen with user profile, stats, categories, priorities, and clear completed button

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from 'react-native'
+import ClearCompletedButton from '@/components/insights/ClearCompletedButton'
+import InsightsCategorySection from '@/components/insights/InsightsCategorySection'
+import InsightsPrioritySection from '@/components/insights/InsightsPrioritySection'
+import InsightsStatsSection from '@/components/insights/InsightsStatsSection'
+import UserProfile from '@/components/insights/UserProfile'
+import TabScreenBackground from '@/components/tabsScreenBackground'
+import { ScrollView } from 'react-native'
 
 const InsightsScreen = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
+    <ScrollView
+      className="flex-1 bg-background py-4"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 20, gap: 14 }}
+      contentInsetAdjustmentBehavior="automatic"
+    >
+      <TabScreenBackground />
+
+      <UserProfile />
+
+      <InsightsStatsSection />
+      <InsightsCategorySection />
+      <InsightsPrioritySection />
+
+      <ClearCompletedButton />
+    </ScrollView>
   )
 }
 
